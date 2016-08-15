143c2202e46e6f70c92ef9e17088b26de4041104
Jean-Baptiste Beck <jb.beck@icloud.com>
2016-08-14 18:47:26 -0700

    cleaning catalog tab

--- app/static/frontend.js
+++ app/static/frontend.js
@@ -1,5 +1,18 @@
-$(document).on("click", ".farm", function() {
-  $('.farm').not($(this)).toggleClass('hide');
+$(document).on("click", ".catalog-farm", function() {
+  $('.catalog-farm').not($(this)).toggleClass('hide');
   $(this).children(".img-container").toggleClass('hide');
   $(this).toggleClass('full-width');
 });
+
+$(document).on("click", ".running-farm", function() {
+  console.log($(this));
+  $('.running-farm').not($(this)).toggleClass('hide');
+  $('.running-farm').toggleClass('full-width');
+  if ($('.running-farm').length > 1 ) {
+    $('.running-farm').not($(this)).toggleClass('hide');
+  }
+  $(this).children(".img-container").toggleClass('hide');
+});
+
+
+
