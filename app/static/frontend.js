598ef49269da2ab41c654cee04e7a3c685a439d8
Jean-Baptiste Beck <jb.beck@icloud.com>
2016-08-15 15:56:05 -0700

    catalog tab

--- app/static/frontend.js
+++ app/static/frontend.js
@@ -1,8 +1,19 @@
+// $(document).on("click", ".li-left", function() {
+//   if $('.li-left').hasClass('active') {
+// //     $('.catalog-farm').toggleClass('hide');
+// //     $('.catalog-farm').children(".img-container").removeClass('hide');
+// //     $('.catalog-farm').removeClass('full-width');
+//   }
+// });
+
 $(document).on("click", ".catalog-farm", function() {
   $('.catalog-farm').not($(this)).toggleClass('hide');
-  $(this).children(".img-container").toggleClass('hide');
+  $(this).children('.farm-body').children('.farm-description').append($(this).children(".img-container").children(".running-farm-logo"));
+  $(this).children(".img-container");
   $(this).toggleClass('full-width');
+  $('h3.farm-name').toggleClass('font-size-lg');
 });
+
 
 $(document).on("click", ".running-farm", function() {
   console.log($(this));
@@ -13,6 +24,4 @@ $(document).on("click", ".running-farm", function() {
   }
   $(this).children(".img-container").toggleClass('hide');
 });
-
-
 
