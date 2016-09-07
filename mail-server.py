9e198f5cdb6de24b4bc94654cbf6c0ad042f914b
Aloys Augustin <aloys.augustin@polytechnique.org>
2016-09-07 15:47:11 +0200

    mail

--- mail-server.py
+++ mail-server.py
@@ -4,7 +4,7 @@ from flask import abort
 import urllib
 import traceback
 import json
-
+import requests
 users = {
     'APIKKJYDM1TVXXREDLWM': 'demos@scalr.com',
     'APIKRJ0I0VGEP1AJP86Z': 'sebastian@scalr.com',
@@ -15,32 +15,44 @@ users = {
 
 app = Flask(__name__)
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
+
 @app.route('/send/',methods=['POST'])
 def handle_query():
     try:
-        data = request.get_json()
+        data = request.get_json(force=True)
+        print "Data:"
+        print data
         param_dict = {}
-        param_dict['user'] = urllib.quote_plus(users[data['user']])
-        param_dict['url'] = urllib.quote_plus(data['url'])
+        param_dict['user'] = urllib.quote(users[data['user']],'')
+        param_dict['url'] = urllib.quote(data['url'],'')
         param_dict['env'] = data['env']
-        param_dict['name'] = urllib.quote_plus(data['appName'])
+        param_dict['name'] = urllib.quote(data['appName'],'')
         param_dict['prio'] = data['perf']
-        param_dict['avai'] = urllib.quote_plus(data['avail'])
-        param_dict['duration'] = urllib.quote_plus(data['duration'])
-        param_dict['farm-id'] = urllib.quote_plus(data['farmId'])
-        url_to_send = "http://disney-portal.demo.scalr.com/approval/#?u={user}&s={url}&e={env}&t={name}&p={prio}&a={avai}&d={duration}&f={farm-id}".format(param_dict)
-        return requests.post(
+        param_dict['avai'] = urllib.quote(data['avail'],'')
+        param_dict['duration'] = urllib.quote(data['duration'],'')
+        param_dict['farm-id'] = urllib.quote(str(data['farmId']),'')
+        param_dict['internet'] = '1' if data['internet'] else '0'
+        url_to_send = "http://disney-portal.demo.scalr.com/approval/#?u={user}&s={url}&e={env}&t={name}&p={prio}&a={avai}&d={duration}&f={farm-id}&i={internet}".format(**param_dict)
+        resp = requests.post(
             "https://api.mailgun.net/v3/sandbox8fdd69ee92db404db4a4454837aad7e4.mailgun.org/messages",
             auth=("api", "key-1a0c7531e47353bd6ca131a8cfeaa018"),
             data={"from": "storefront@scalr.com",
-                  "to": ["mohammed@scalr.com"],
-                  "subject": "Approval required",
-                  "text": url_to_send})
+                  "to": ["aloys@scalr.com"],
+                  "subject": "Storefront approval required",
+                  "text": "Hello,\n" + "Your approval is required. Please go to the following address to review:\n" + url_to_send,
+                  "html": "Hello,\n<br>\n" + "Your approval is required for a new "+data['appName']+" applicatin. Please click the following link to review:\n" + '<a href="' + url_to_send + '"> Review here </a>'})
+        return "OK!"
     except Exception as e:
         print(e)
         traceback.print_exc()
-        abort(404)
+        abort(401)
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
