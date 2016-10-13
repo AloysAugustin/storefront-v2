dc0daeb197e43d9e8f18a364fefcdb3a0cd004a4
Aloys Augustin <aloys.augustin@polytechnique.org>
2016-10-13 13:50:37 +0200

    Move recipe to recipes.js & delete old one

--- app/recipes.js
+++ app/recipes.js
@@ -3,52 +3,91 @@ var app = angular.module('ScalrStorefront');
 app.factory("recipes", ["apiRecipes",function(apiRecipes){
     apiRecipes.register('ubuntu', {
         data: {
- 
+            initialFarmId: 184
         },
-        validateParams: function(data, params) {
-            var paramsList = ['envId', 'cloudImageId', 'name'];
-            for (p in paramsList) {
-                if (!p in params) {
-                    return false;
-                }
-            }
-            return true;
-        },
+        validateParams: apiRecipes.mkValidateParams(['keyId']),
         steps: [
             {
-                description: 'Create image',
+                description: 'Clone base farm',
                 method: 'POST',
                 url: function(data, params) {
-                    return '/api/v1beta0/user/{envId}/images/'.replace('{envId}', params.envId);
+                    return '/api/v1beta0/user/{envId}/farms/{farmId}/actions/clone/'.replace('{envId}', params.envId).replace('{farmId}', data.initialFarmId);
                 },
                 body: function(data, params) {
                     return JSON.stringify({
-                        architecture: 'i386',
-                        cloudImageId: params.cloudImageId,
-                        cloudPlatform: 'ec2',
-                        name: params.name,
-                        os: {
-                             id: 'ubuntu-16-04'
-                        },
-                        cloudInitInstalled: true,
-                        cloudLocation: 'us-east-1',
-                        scalrAgentInstalled: false
+                        'name': '[' + params.keyId + ']' + params.name
                     });
                 },
                 done: function(response, data, params) {
-                    data.createdImage = reponse.data;
+                    data.newFarm = response.data;
                 },
                 undo: {
                     method: 'DELETE',
                     url: function(data, params) {
-                        return '/api/v1beta0/user/{envId}/images/{imageId}/'.replace('{envId}', params.envId).replace('{imageId}', data.createdImage.id);
+                        return '/api/v1beta0/user/{envId}/farms/{farmId}/'.replace('{envId}', params.envId).replace('{farmId}', data.newFarm.id);
                     }
-                },
+                }
             },
             {
-
+                description: 'Set farm description',
+                method: 'PATCH',
+                url: function(data, params) {
+                    return '/api/v1beta0/user/{envId}/farms/{farmId}/'.replace('{envId}', params.envId).replace('{farmId}', data.newFarm.id);
+                },
+                body: function(data, params) {
+                    var settings = angular.copy(params);
+                    delete settings.keyId;
+                    return JSON.stringify({
+                        description: JSON.stringify({
+                            settings: settings
+                        }),
+                    });
+                },
+                done: function(response, data, params) {},
+            },
+            {
+                description: 'Get new farm role',
+                method: 'GET',
+                url: function(data, params) {
+                    return '/api/v1beta0/user/{envId}/farms/{farmId}/farm-roles/'.replace('{envId}', params.envId).replace('{farmId}', data.newFarm.id);
+                },
+                done: function(response, data, params) {
+                    data.newFarmRoles = response.data;
+                }
+                // Nothing to undo
+            },
+            {
+                description: 'Set new farm role instance type',
+                method: 'PATCH',
+                url: function(data, params) {
+                    return '/api/v1beta0/user/{envId}/farm-roles/{farmRoleId}/instance/'.replace('{envId}', params.envId).replace('{farmRoleId}', data.newFarmRoles[0].id);
+                },
+                body: function(data, params) {
+                    var instanceType = {
+                        _01small: "m3.medium",
+                        _02medium: "m3.large",
+                        _03large: "m3.xlarge",
+                    }[params.flavor];
+                    return JSON.stringify({
+                        instanceConfigurationType: "AwsInstanceConfiguration",
+                        instanceType: {
+                          id: instanceType
+                        }
+                    });
+                },
+                done: function(response, data, params) {},
+                // The Farm Role will be deleted with the farm, nothing to undo
+            },
+            {
+                description: 'Launch farm',
+                method: 'POST',
+                url: function(data, params) {
+                    return '/api/v1beta0/user/{envId}/farms/{farmId}/actions/launch/'.replace('{envId}', params.envId).replace('{farmId}', data.newFarm.id);
+                },
+                done: function(response, data, params) {},
             }
         ]
-    });
+
+    });    
 }])
 
