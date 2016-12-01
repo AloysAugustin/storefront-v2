6b663759f53a0364ada763aaf830a4b7d2b7b729
Mohammed Hawari <mohammed@hawari.fr>
2016-12-01 15:07:56 +0100

    Fix JSON

--- mail-server.py
+++ mail-server.py
@@ -28,8 +28,8 @@ def handle_query():
         param_dict['env'] = data['env']
         param_dict['name'] = urllib.quote(data['appName'],'')
         param_dict['farm-id'] = urllib.quote(str(data['farmId']),'')
-        param_dict['params'] = urllib.quote(str(data['params']), '')
-        url_to_send = "http://portal.demo.scalr.com/approval/#?u={user}&s={url}&e={env}&t={name}&f={farm-id}&p={params}".format(**param_dict)
+        param_dict['params'] = urllib.quote(json.dumps(data['params']), '')
+        url_to_send = "http://localhost:8008/approval/#?u={user}&s={url}&e={env}&t={name}&f={farm-id}&p={params}".format(**param_dict)
         resp = requests.post(
             "https://api.mailgun.net/v3/sandbox8fdd69ee92db404db4a4454837aad7e4.mailgun.org/messages",
             auth=("api", "key-1a0c7531e47353bd6ca131a8cfeaa018"),
