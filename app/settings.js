4566bd911e1557ead3de4b324ea0f851c4cfab5b
Mohammed Hawari <mohammed@hawari.fr>
2017-01-11 13:43:03 +0100

    demo.scalr.com in https

--- app/settings.js
+++ app/settings.js
@@ -1,8 +1,8 @@
 var app = angular.module('ScalrStorefront');
 app.factory('settings', function() {
     return {
-        'apiV2Url' : "http://demo.scalr.com/",
-        'oAuthGrantUrl' : "http://demo.scalr.com/public/oauth",
+        'apiV2Url' : "https://demo.scalr.com/",
+        'oAuthGrantUrl' : "https://demo.scalr.com/public/oauth",
         'oAuthClientId' : "56866536e49f",
         'oAuthRedirectUrl' : "http://oauth.demo.scalr.com/"
     };
